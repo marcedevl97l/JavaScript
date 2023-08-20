d20838394fcddb9e2dc4a304c184d4412c46e968
Marcelo Sebastian <cerealkill05@gmail.com>
2023-08-20 18:46:20 -0500

    se agrego reto 60 dias

---
+++ Sesiones/if-else.js
@@ -0,0 +1,12 @@
+let saldo = 40;
+let efectivo = 20;
+
+if(efectivo < saldo){
+  console.log("Puedes sacar dinero")
+}
+
+if(efectivo < saldo){
+  console.log("Puedes sacar dinero")
+} else {
+  console.log("Saldo insuficiente")
+}
---
+++ Sesiones/sentencias.js
@@ -0,0 +1,8 @@
+// Sentencias switch
+
+let nota = 5;
+
+switch (nota) {
+  case 5: 
+    console.log("Buen trabajo sobresaliente")
+}
