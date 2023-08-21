b5442c722f74f320c6c36aa2e070e42e9e7044c1
Marcelo Sebastian <cerealkill05@gmail.com>
2023-08-20 23:00:49 -0500

    Dia1 Dia2 Dia3

---
+++ Reto Javascritp 60 dias/Dia 2 Variables y tipos de datos.js
@@ -0,0 +1,35 @@
+// Variables y Tipos de datos en Javascript
+
+//Las variables son contenedores donde puedes almacenar valores
+
+let nombredelaVariable; //es mas recomendable utilizar let que var en nuestro codigo
+
+let nombre = "Marcelo";
+//obtenemos el valor de la variable llamandola por su nombre
+nombre = "marcelo" //despues de asignarle un valor a una variable podemos cambiarlo
+
+//Tipos de datos
+
+// String = cadena de caracteres
+let miVariable = "Bob";
+
+//Number
+let numberVariable = "10";
+
+//Booleano
+let trueorfalseVariable = "true";
+let trueorFalseVariable = "false";
+
+//Array Estructura que permite almacenar varios valores en una sola referencia 
+let miVAriable = [1, "holamundo", false,];
+miVAriable [0];
+
+//Object Todo en JavaScript es un objeto y puede ser almacenado en una variable, Manten siempre en mente esto mientras aprendes 
+document = "documento";
+//let objetoVariable = document.querySelector('h1'); //Todos los ejemplos anteriores tambien
+
+/* Esto es un 
+comentario 
+multilinea papu
+*/
+
